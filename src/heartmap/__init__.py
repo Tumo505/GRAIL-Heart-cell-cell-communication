eb211408fe5165c536af6db41890d429e446238d
tumo505 <tumokgabeng@gmail.com>
2025-08-06 18:41:09 +0200

    fix test

--- src/heartmap/__init__.py
+++ src/heartmap/__init__.py
@@ -6,15 +6,14 @@ of the human heart using single-cell RNA sequencing data.
 """
 
 __version__ = "1.0.0"
-__author__ = "Tumo Kgabeng, Thanyani Pandelani, Lulu Wang, Harry Ngwangwa"
+__author__ = "Tumo Kgabeng, Lulu Wang, Harry Ngwangwa, Thanyani Pandelani"
 __email__ = "28346416@mylife.unisa.ac.za"
 
-from .models import HeartMapModel
+#from .models import HeartMapModel
 from .pipelines import BasicPipeline, AdvancedCommunicationPipeline, MultiChamberPipeline
 from .config import Config
 
 __all__ = [
-    "HeartMapModel",
     "BasicPipeline", 
     "AdvancedCommunicationPipeline",
     "MultiChamberPipeline",
